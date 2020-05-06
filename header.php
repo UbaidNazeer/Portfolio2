b2679156452a926b44319c7b2a80ebb6a459b473
UbaidNazeer <64394376+UbaidNazeer@users.noreply.github.com>
2020-05-06 12:34:22 +0500

    initial

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  
+</head>
+<body>
+<div class="header" id="top-header">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container text-center text-uppercase p-2">
+            <a href="#" class="navbar-brand">Porfolio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navcollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="#" class="nav-link">Portfolio</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Team</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+    
+     
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>       
+</body>
