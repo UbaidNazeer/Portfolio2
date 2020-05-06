3e4b5ad8a9bb5b03bbb0205974d9a603917122d0
UbaidNazeer <64394376+UbaidNazeer@users.noreply.github.com>
2020-05-06 16:24:09 +0500

    initial

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php include "header.php"?>
+<!DOCTYPE HTML>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <script src="https://kit.fontawesome.com/55a0398826.js" crossorigin="anonymous"></script>
 
+
+</head>
+<body>
+         <section>
+             <div class="container extra-div">
+                 <div class="row">
+                     <div class="col-lg-4 col-md-4 col-sm-4 d-block m-auto">
+                        <i class="fa fa-3x fa-desktop"></i>
+                        <h1>Desktop</h1>
+                        <p>We are professional developers.
+                        We are professional developers.
+                        We are professional developers.</p>
+                         
+                     </div>
+                     <div class="col-lg-4 col-md-4 col-sm-4 d-block m-auto">
+                        <i class="fa fa-3x fa-desktop"></i>
+                        <h1>Desktop</h1>
+                        <p>We are professional developers.
+                        We are professional developers.
+                        We are professional developers.</p>
+                         
+                     </div>
+                     <div class="col-lg-4 col-md-4 col-sm-4 d-block m-auto">
+                        <i class="fa  fa-3x fa-desktop"></i>
+                        <h1>Desktop</h1>
+                        <p>We are professional developers.
+                        We are professional developers.
+                        We are professional developers.</p>
+                         
+                     </div>
+                 </div>
+             </div>
+         </section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> 
+</body>
 
 
 
