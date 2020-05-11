e5f33ce401ff9eddd86fcc8d35aec30514fd09a3
UbaidNazeer <64394376+UbaidNazeer@users.noreply.github.com>
2020-05-11 15:21:19 +0500

    initial

--- index.php
+++ index.php
@@ -1,48 +1,120 @@
 <?php include "header.php"?>
 <!DOCTYPE HTML>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-      <script src="https://kit.fontawesome.com/55a0398826.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://kit.fontawesome.com/55a0398826.js" crossorigin="anonymous"></script>
 
 
 </head>
 <body>
-         <section class="extra-div">
-             <div class="container">
-                 <div class="row">
-                     <div class=" ex-div col-lg-4 col-md-4 col-12">
-                        <i class="fa fa-3x fa-desktop"></i>
-                        <h1>Desktop</h1>
-                        <p>We are professional developers.
-                        We are professional developers.
-                        We are professional developers.</p>
-                         
-                     </div>
-                     <div class="ex-div col-lg-4 col-md-4 col-12">
-                        <i class="fa fa-3x fa-trophy"></i>
-                        <h1>Awesome Design</h1>
-                        <p>We are professional developers.
-                        We are professional developers.
-                        We are professional developers.</p>
-                         
-                     </div>
-                     <div class="ex-div col-lg-4 col-md-4 col-12">
-                        <i class="fa  fa-3x fa-magic"></i>
-                        <h1>Easy to Customize</h1>
-                        <p>We are professional developers.
-                        We are professional developers.
-                        We are professional developers.</p>
-                         
-                     </div>
-                 </div>
-             </div>
-         </section>
+<section class="extra-div">
+<div class="container">
+<div class="row">
+<div class=" ex-div col-lg-4 col-md-4 col-12">
+<i class="fa fa-3x fa-desktop"></i>
+<h1>Desktop</h1>
+<p>We are professional developers.
+We are professional developers.
+We are professional developers.</p>
+
+</div>
+<div class="ex-div col-lg-4 col-md-4 col-12">
+<i class="fa fa-3x fa-trophy"></i>
+<h1>Awesome Design</h1>
+<p>We are professional developers.
+We are professional developers.
+We are professional developers.</p>
+
+</div>
+<div class="ex-div col-lg-4 col-md-4 col-12">
+<i class="fa  fa-3x fa-magic"></i>
+<h1>Easy to Customize</h1>
+<p>We are professional developers.
+We are professional developers.
+We are professional developers.</p>
+
+</div>
+</div>
+</div>
+</section>
+<section class="dev">
+<div class="container-fluid bg-light p-2">
+<h1 class="text-center">What we offers</h1>
+
+<div class="container bg-light p-2">
+<div class="row">
+<div class="col-lg-6 col-md-12 col-10">
+<div class="names">
+    <h1 class="">HTML</h1>
+</div>
+<div class="progress w-75">
+<div class="progress-bar bg-info progress-bar-striped progress-bar-animated" style="width:100%">100%</div>
+</div>
+<div class="names">
+    <h1 class="">CSS</h1>
+</div>
+<div class="progress w-75">
+<div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" style="width:80%">80%</div>
+</div>
+<div class="names">
+    <h1 class="">JAVASCRIPT</h1>
+</div>
+<div class="progress w-75">
+<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width:70%">70%</div>
+</div>
+<div class="names">
+    <h1 class="">NODE.JS</h1>
+</div>
+<div class="progress w-75">
+<div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width:50%">50%</div>
+</div>
+
+</div>
+<div class="col-lg-6 col-md-12 col-10">
+   <div class="row">
+       <div class="col-lg-2 col-2">
+           <i class="fa  fa-3x fa-desktop"></i>
+       </div>
+       <div class="col-lg-10 col-10">
+           <h1>Website Development</h1>
+           <p>We have a team of professional developers.
+           We have a team of professional developers.</p>
+       </div>
+   </div>
+   <div class="row">
+       <div class="col-lg-2 col-2">
+           <i class="fa  fa-3x fa-wifi"></i>
+       </div>
+       <div class="col-lg-10 col-10">
+           <h1>Digital Marketing</h1>
+           <p>We have a team of professional developers.
+           We have a team of professional developers.</p>
+       </div>
+   </div>
+   <div class="row">
+       <div class="col-lg-2 col-2">
+           <i class="fa  fa-3x fa-phone"></i>
+       </div>
+       <div class="col-lg-10 col-10">
+           <h1>Support 24/7</h1>
+           <p>We have a team of professional developers.
+           We have a team of professional developers.</p>
+       </div>
+   </div>
+   
+    </div>
+</div>
+</div>
+</div>
+
+
+</section>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> 
 </body>
 
 
